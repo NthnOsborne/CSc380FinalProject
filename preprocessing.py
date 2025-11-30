6db81d6b29f474d774785ef93288a17dd167e055
NthnOsborne <nateosb159@gmail.com>
2025-11-30 13:34:51 -0700

    preprocessinggggg

---
+++ preprocessing.py
@@ -0,0 +1,41 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+stop_words = set(stopwords.words('english'))
+def remove_stop_words(inp):
+    words = nltk.word_tokenize(inp)
+    no_stop_words = []
+    for word in words:
+        if word not in stop_words:
+            no_stop_words.append(word)
+    return ' '.join(no_stop_words)
+
+lemmatizer = WordNetLemmatizer()
+def lemmatize_text(inp):
+    words = nltk.word_tokenize(inp)
+    lem_words = []
+    for word in words:
+        lem_words.append(lemmatizer.lemmatize(word))
+    return ','.join(lem_words)
+
+data = pd.read_csv('edos_labelled_data.csv')
+
+# Lowercase
+data['text'] = data['text'].str.lower()
+# URLs
+data['text'] = data['text'].str.replace(r'http\S+|www\S+', '', regex=True)
+# HTML tags
+data['text'] = data['text'].str.replace(r'<.*?>', '', regex=True)
+# Any remaining non alphanumerical characters(emojis, puncuation)
+data['text'] = data['text'].str.replace(r'[^a-zA-Z0-9 ]', '', regex=True)
+# Remove stop words
+data['text'] = data['text'].apply(remove_stop_words)
+# Lemmatize
+data['text'] = data['text'].apply(lemmatize_text)
+
+print(data['text'])
